5e613c63e05b3c12be69182d62bb0625c582ba5b
blancafernandezmartin <72817601+blancafernandezmartin@users.noreply.github.com>
2021-11-23 13:08:22 +0100

    corregido

--- main.c
+++ main.c
@@ -108,135 +108,83 @@ int main(void)
   //encendido el led verde solo
   led_green_toggle();
   
+
   while(1){
-  	switch(state){
-  		case 0:
-  		//las dos puertas cerradas. led verde encendido, led rojo apagado
-  			if (sw1_check()){
-  				if(sw2_check()){
-  					//se abren las dos a la vez
-  					state = 3;
-  					p1 = !p1;
-  					p2 = !p2;
-  					//encender led rojo
-  					led_red_toggle();
-  					//apagar verde
-  					led_green_toggle();
-  					while (sw2_check());
-  				}else{
-  					//se abre solo la 1
-  					state = 2;
-  					p1 = !p1;
-  					//encender rojo
-  					led_red_toggle();
-  					//apagar verde
-  					led_green_toggle();
-  				};
+  	if (sw1_check()){
+  		switch(state){
+  			case 0:
+  				//se abre solo la 1
+  				state = 2;
+  				p1 = !p1;
+  				//encender rojo
+  				led_red_toggle();
+  				//apagar verde
+  				led_green_toggle();
   				while (sw1_check());
-  			}else {
-  				if (sw2_check()){
-  					//se abre solo la 2
-  					state = 1;
-  					p2 = !p2;
-  					//encender rojo
-  					led_red_toggle();
-  					//apagar verde
-  					led_green_toggle();
-  					while (sw2_check());
-  				};
-  				//no se abre ninguna, no se pulso nada no hace nada
-  			}
-  		case 1:
-  		//primera puerta cerrada, segunda abierta. led verde apagado, led rojo encendido
-  			if (sw1_check()){
-  				if(sw2_check()){
-  					//se abre la primera y se cierra la segunda
-  					state = 2;
-  					p1 = !p1;
-  					p2 = !p2;
-  					while (sw2_check());
-  				}else{
-  					//se abre la 1, las dos abiertas
-  					state = 3;
-  					p1 = !p1;
-  				};
+  				break;
+  			case 1:
+  				//se abre la 1, las dos abiertas
+  				state = 3;
+  				p1 = !p1;
   				while (sw1_check());
-  			}else {
-  				if (sw2_check()){
-  					//se cierra la segunda, las dos estan cerradas
-  					state = 0;
-  					p2 = !p2;
-  					//encender verde
-  					led_green_toggle();
-  					//apagar rojo
-  					led_red_toggle();
-  					while (sw2_check());
-  				};
-  				//no se pulso nada no hace nada
-  			}
-  			
-  			
-  		case 2:
-  		//primera puerta abierta, segunda cerrada. led verde apagado, led rojo encendido
-  			if (sw1_check()){
-  				if(sw2_check()){
-  					//se abre habre la segunda y se cierra la primera
-  					state = 1;
-  					p1 = !p1;
-  					p2 = !p2;
-  					while (sw2_check());
-  				}else{
-  					//se cierra la 1, las dos cerradas
-  					state = 0;
-  					p1 = !p1;
-  					//encender verde
-  					led_green_toggle();
-  					//apagar rojo
-  					led_red_toggle();
-  				};
+  				break;
+  			case 2:
+  				//se cierra la 1, las dos cerradas
+  				state = 0;
+  				p1 = !p1;
+  				//encender verde
+  				led_green_toggle();
+  				//apagar rojo
+  				led_red_toggle();
   				while (sw1_check());
-  			}else {
-  				if (sw2_check()){
-  					//se abre la segunda, las dos estan abiertas
-  					state = 3;
-  					p2 = !p2;
-  					while (sw2_check());
-  				};
-  				//no se pulso nada no hace nada
-  			}
-  			
-  		case 3:
-  		//dos puertas abiertas, led rojo encendido
-  			if (sw1_check()){
-  				if(sw2_check()){
-  					//se cierran las dos
-  					state = 0;
-  					p1 = !p1;
-  					p2 = !p2;
-  					//encender led verde
-  					led_green_toggle();
-  					//apagar rojo
-  					led_red_toggle();
-  					while (sw2_check());
-  				}else{
-  					//se cierra la 1, la segunda abierta
-  					state = 1;
-  					p1 = !p1;
-  				};
+  				break;
+  			case 3:
+  				//se cierra la 1, la segunda abierta
+  				state = 1;
+  				p1 = !p1; 
   				while (sw1_check());
-  			}else {
-  				if (sw2_check()){
-  					//se cierra la segunda, la primera abierta
-  					state = 2;
-  					p2 = !p2;
-  					while (sw2_check());
-  				};
-  				//no se pulso nada no hace nada
-  			}
-  			
-  			
+  				break;			
+  		}
+  	}
+  	
+  	if(sw2_check()){
+  		switch(state){
+  			case 0:
+  				//se abre solo la 2
+  				state = 1;
+  				p2 = !p2;
+  				//encender rojo
+  				led_red_toggle();
+  				//apagar verde
+  				led_green_toggle();
+  				while (sw2_check());
+  				break;
+  			case 1:
+  				//se cierra la segunda, las dos estan cerradas
+  				state = 0;
+  				p2 = !p2;
+  				//encender verde
+  				led_green_toggle();
+  				//apagar rojo
+  				led_red_toggle();
+  				while (sw2_check());
+  				break;
+  			case 2:
+  				//se abre la segunda, las dos estan abiertas
+  				state = 3;
+  				p2 = !p2;
+  				while (sw2_check());
+  				break;
+  			case 3:
+  				//se cierra la segunda, la primera abierta
+  				state = 2;
+  				p2 = !p2;
+  				while (sw2_check());
+  				break;		
+  		}
   	}
 
-  }
+  };
+
   return 0;
 }
